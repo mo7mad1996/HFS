913a64e74fb247fb8151488b5a5968a67d42cf51
Mohamed Ibrahim <mo7mad369@gmail.com>
2024-12-16 17:21:58 +0200

    edit footer design...

--- src/Components/Home/Footer/Footer.jsx
+++ src/Components/Home/Footer/Footer.jsx
@@ -14,9 +14,11 @@ import {
 import { useState } from "react";
 import logo from "@/assets/images/HFSLOGO.png";
 
+import css from "./style.module.css";
+
 // import brand from "../../assets/brand.png";
 import instagramIcon from "@/assets/images/instagram.png";
-import { FaFacebookF, FaInstagram } from "react-icons/fa";
+import { FaFacebookF, FaInstagram, FaYoutube } from "react-icons/fa";
 import { IoCall } from "react-icons/io5";
 import { FaMap, FaEnvelope } from "react-icons/fa";
 import emailjs from "emailjs-com";
@@ -83,96 +85,69 @@ function Footer() {
       }}
     >
       <Container>
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            flexDirection: { xs: "column", md: "row" },
-            justifyContent: { xs: "center", md: "flex-start" },
-            mb: { xs: 4, md: 0 },
-            textAlign: { xs: "center", md: "left" },
-            transform: { lg: "translate(-60px,-30px)" },
-          }}
-        >
-          <Box
-            component="img"
-            src={logo}
-            sx={{ mb: { xs: 2, md: 0 }, width: "69px", height: "69px" }}
-          />
-          {/* <Box
-            component="img"
-            src={brand}
-            sx={{
-              width: { xs: "120px", md: "153px" },
-              ml: { xs: 0, md: 2 },
-            }}
-          /> */}
-        </Box>
-
         <Grid2 container spacing={2} sx={{ mt: "10px" }}>
-          <Grid2 size={{ xs: 12, md: 4, lg: 3.3 }}>
-            <Box
-              sx={{
-                color: "gray",
-                fontSize: { xs: "14px", md: "18px", xl: "25px" },
-                // fontFamily: "TanseekModernProArabic-ExBold",
-                textAlign: { xs: "center", md: "left" },
-                mb: { xs: 4, md: 0 },
-              }}
-            >
-              <Typography>Empowering traders with real-time tools,</Typography>
-              <Typography>expert education, and a strong </Typography>
-              and a strong
-              <Typography>community. </Typography>
-            </Box>
-
+          <Grid2 size={{ xs: 12, md: 4 }}>
             <Box
               sx={{
                 display: "flex",
-                gap: 2,
+                flexDirection: { xs: "column", md: "row" },
+                justifyContent: { xs: "center", md: "flex-start" },
+                mb: { xs: 4, md: 0 },
+                textAlign: { xs: "center", md: "left" },
+                gap: "1em",
+                // transform: { lg: "translate(-60px,-30px)" },
               }}
             >
               <Box
-                sx={{
-                  color: "#fff",
-                  width: "25px",
-                  height: "25px",
-                  backgroundColor: "#0a83ed",
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  borderRadius: "50%",
-                }}
-              >
-                <FaFacebookF />
-              </Box>
-              <Box
                 component="img"
-                src={instagramIcon}
-                sx={{ width: "25px", height: "25px" }}
+                src={logo}
+                sx={{ mb: { xs: 2, md: 0 }, height: "50px" }}
               />
               <Box
                 sx={{
-                  color: "text.primary",
-                  width: "25px",
-                  height: "25px",
-                  backgroundColor: "#0a83ed",
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  borderRadius: "50%",
+                  color: "gray",
+                  fontSize: { xs: "14px", md: "18px", xl: "25px" },
+                  // fontFamily: "TanseekModernProArabic-ExBold",
+                  textAlign: { xs: "center", md: "left" },
+                  mb: { xs: 4, md: 0 },
                 }}
               >
-                <FaInstagram />
+                <Typography>
+                  Empowering traders with real-time tools,
+                </Typography>
+                <Typography>
+                  expert education, and a strong <b>and a strong</b>
+                </Typography>
+
+                <Typography>community. </Typography>
               </Box>
+            </Box>
+
+            <Box className={css.social_links}>
+              <a href="#" target="_blank">
+                <FaFacebookF
+                  className={css._icon}
+                  style={{ color: "#4267B2" }}
+                />
+              </a>
+
+              <a href="#" target="_blank">
+                <FaYoutube className={css._icon} style={{ color: "#FF0000" }} />
+              </a>
+              <a href="#" target="_blank">
+                <FaInstagram
+                  className={css._icon}
+                  style={{ color: "#7b2672" }}
+                />
+              </a>
             </Box>
           </Grid2>
 
-          <Grid2 size={{ xs: 12, md: 4, lg: 3 }}>
+          <Grid2 size={{ xs: 12, md: 4 }}>
             <Typography
+              className={css.sectionTitle}
               sx={{
                 fontSize: { xs: "16px", md: "20px", xl: "22px" },
-                color: "#ECBC56",
               }}
             >
               Quick Links
@@ -180,193 +155,48 @@ function Footer() {
 
             <Box
               sx={{
-                display: "flex",
-                gap: "12px",
-                listStyleType: "none",
-                // fontFamily: "TanseekModernProArabic-ExBold",
                 fontSize: { xs: "20px", md: "25px", xl: "30px" },
+                display: "flex",
               }}
             >
               <Box>
-                <Box>About Us</Box>
-                <Box>Support</Box>
-                <Box>Academy</Box>
+                <Typography className={css.item}>About Us</Typography>
+                <Typography className={css.item}>Support</Typography>
+                <Typography className={css.item}>Academy</Typography>
               </Box>
               <Box>
-                <Box>Terms and Conditions</Box>
-                <Box>Privacy Policy</Box>
+                <Typography className={css.item}>
+                  Terms and Conditions
+                </Typography>
+                <Typography className={css.item}>Privacy Policy</Typography>
               </Box>
             </Box>
           </Grid2>
 
-          <Grid2 size={{ xs: 12, md: 4, lg: 2 }}>
+          <Grid2 size={{ xs: 12, md: 4 }}>
             <Typography
               sx={{
                 fontSize: { xs: "16px", md: "20px", xl: "22px" },
-                color: "#ECBC56",
-                letterSpacing: "-0.17px",
               }}
+              className={css.sectionTitle}
             >
               Get in Touch
             </Typography>
 
-            <Box sx={{ display: "flex", alignItems: "center", gap: "10px" }}>
-              <Box
-                sx={{
-                  backgroundColor: "#ECBC56",
-                  borderRadius: "50%",
-                  width: "30px",
-                  height: "30px",
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
-              >
-                <IoCall style={{ color: "#000" }} />
-              </Box>
+            <Box className={css.item}>
+              <IoCall className={css.icon} />
               <Typography>971 - 555555555</Typography>
             </Box>
-            <Box sx={{ display: "flex", alignItems: "center", gap: "10px" }}>
-              <Box sx={{ width: "25px", height: "25px" }}>
-                <FaMap
-                  style={{ color: "#ECBC56", width: "100%", height: "100%" }}
-                />
-              </Box>
+            <Box className={css.item}>
+              <FaMap className={css.icon} />
               <Typography>Dubai - UAE</Typography>
             </Box>
-            <Box sx={{ display: "flex", alignItems: "center", gap: "10px" }}>
-              <Box sx={{ width: "25px", height: "25px" }}>
-                <FaEnvelope
-                  style={{ color: "#ECBC56", width: "100%", height: "100%" }}
-                />
-              </Box>
+            <Box className={css.item}>
+              <FaEnvelope className={css.icon} />
               <Typography>info@tradingsociety.net</Typography>
             </Box>
           </Grid2>
-
-          <Grid2
-            sx={{
-              position: "relative",
-              height: "100%",
-              mx: "auto",
-            }}
-            size={{ xs: 12, md: 4, lg: 3.6 }}
-          >
-            <TextField
-              margin="dense"
-              placeholder="Your suggestion"
-              variant="outlined"
-              slotProps={{
-                input: {
-                  style: {
-                    borderRadius: "30px",
-                    width: "334px",
-                    height: "44px",
-                    color: "#000",
-                    backgroundColor: "#fff",
-                    border: "none",
-                  },
-                },
-              }}
-              onClick={handleClickOpen}
-            />
-            <Button
-              sx={{
-                position: "absolute",
-                width: "103px",
-                height: "32px",
-                borderRadius: "50px",
-                backgroundColor: "#ECBC56",
-                color: "#000",
-                top: "52%",
-                transform: "translateY(-50%)",
-                right: { xs: "40px", md: "10px" },
-                fontSize: "15px",
-                letterSpacing: "-0.17px",
-                lineHeight: "18px",
-                textTransform: "capitalize",
-              }}
-            >
-              Click Here
-            </Button>
-          </Grid2>
         </Grid2>
-
-        <Dialog open={open} onClose={handleClose}>
-          <DialogTitle>Contact Us</DialogTitle>
-          <DialogContent>
-            <TextField
-              autoFocus
-              margin="dense"
-              label="Name"
-              fullWidth
-              variant="outlined"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              slotProps={{
-                input: {
-                  style: { color: "#000" },
-                },
-              }}
-            />
-            <TextField
-              margin="dense"
-              label="Age"
-              fullWidth
-              variant="outlined"
-              value={age}
-              onChange={(e) => setAge(e.target.value)}
-              slotProps={{
-                input: {
-                  style: { color: "#000" },
-                },
-              }}
-            />
-            <TextField
-              margin="dense"
-              label="Email"
-              fullWidth
-              variant="outlined"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              slotProps={{
-                input: {
-                  style: { color: "#000" },
-                },
-              }}
-            />
-            <TextField
-              margin="dense"
-              label="Phone"
-              fullWidth
-              variant="outlined"
-              value={phone}
-              onChange={(e) => setPhone(e.target.value)}
-              slotProps={{
-                input: {
-                  style: { color: "#000" },
-                },
-              }}
-            />
-            <TextField
-              margin="dense"
-              label="Comment"
-              fullWidth
-              variant="outlined"
-              value={comment}
-              onChange={(e) => setComment(e.target.value)}
-              slotProps={{
-                input: {
-                  style: { color: "#000" },
-                },
-              }}
-            />
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleClose}>Cancel</Button>
-            <Button onClick={handleSubmit}>Submit</Button>
-          </DialogActions>
-        </Dialog>
       </Container>
 
       <Divider sx={{ backgroundColor: "#C3AD57", my: "12px" }} />
